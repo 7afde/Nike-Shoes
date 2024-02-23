dc371568c695b029d95fd64e4d5bf83df74ea755
hafed <hafed.zouyed@univ-constantine2.dz>
2024-02-23 19:23:35 +0100

    I added header section

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative xl:h-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p>Our Summer Collection</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/sections/Services.jsx
@@ -0,0 +1,5 @@
+const Services = () => {
+  return <div>woi</div>;
+};
+
+export default Services;
